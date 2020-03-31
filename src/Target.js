3a2d1fed312bb11add226a8c4556ae3b8660ec46
James Semple <jamessemple@SE-James-MBP.local>
2020-03-31 14:58:19 +0100

    Target converted to use Hooks

--- src/Target.js
+++ src/Target.js
@@ -4,31 +4,22 @@ import Card from '@material-ui/core/Card';
 import StorefrontIcon from '@material-ui/icons/Storefront';
 import './Target.css';
 
-class Target extends React.Component {
-  constructor(props) {
-    super(props);
+const Target = (props) => {
+  const buttonClick = () => {
+    props.cb(props.slug);
+  };
 
-    this.state = {click: this.props.cb};
-    this.buttonClick = this.buttonClick.bind(this);
-  }
-
-  buttonClick() {
-    this.props.cb(this.props.slug);
-  }
-
-  render() {
-    return (
-      <Card className="c-target">
-        <h1>{this.props.name}</h1>
-        <p>{this.props.region}</p>
-        <a href={this.props.link}>
-          <StorefrontIcon fontSize='large' color='primary' />
-        </a>
-        <p>Deployed at {this.props.deploy}</p>
-        <Button className="c-target__delete" color="secondary" onClick={this.buttonClick}>DELETE</Button>
-      </Card>
-    );
-  }
+  return (
+    <Card className="c-target">
+      <h1>{props.name}</h1>
+      <p>{props.region}</p>
+      <a href={props.link}>
+        <StorefrontIcon fontSize='large' color='primary' />
+      </a>
+      <p>Deployed at {props.deploy}</p>
+      <Button className="c-target__delete" color="secondary" onClick={buttonClick}>DELETE</Button>
+    </Card>
+  );
 }
-  
+
 export default Target;

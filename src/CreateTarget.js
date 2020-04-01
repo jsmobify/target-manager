4e6cca2a3802eea02ba297ed58bde7f8568f1200
James Semple <jamessemple@SE-James-MBP.local>
2020-04-01 11:48:37 +0100

    Updated CreateTarget (correctly) to use Hooks

--- src/CreateTarget.js
+++ src/CreateTarget.js
@@ -1,44 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
 import Card from '@material-ui/core/Card';
 import './CreateTarget.css';
 
-class CreateTarget extends React.Component {
-    constructor(props) {
-        super(props);
+const CreateTarget = (props) => {
+    const [targetName, setTargetName] = useState('');
+    const [targetSlug, setTargetSlug] = useState('');
 
-        this.state = {
-                        targetName: '',
-                        targetSlug: '', 
-                        click: props.cb 
-                     };
-
-        this.inputChange = this.inputChange.bind(this);
-        this.buttonClick = this.buttonClick.bind(this);
-    }
-
-    inputChange(e) {
-        this.setState({[e.target.name]: e.target.value});
-    }
-
-    buttonClick () {
-        if (this.state.targetName.length > 0 && this.state.targetSlug.length > 0) {
-            this.setState({targetName: '', targetSlug: ''});
-            this.state.click(this.state.targetName, this.state.targetSlug);
+    const buttonClick = () => {
+        if (targetName.length > 0 && targetSlug.length > 0) {
+            props.cb(targetName, targetSlug);
+            setTargetName('');
+            setTargetSlug('');
         }
     }
 
-    render() {
-        return (
+    return (
         <Card className="c-createTarget">
             <h1>New Target</h1>
-            <Input type="text" name="targetName" value={this.state.targetName} placeholder="Target Name" onChange={this.inputChange} />
-            <Input type="text" name="targetSlug" value={this.state.targetSlug} placeholder="Target Slug" onChange={this.inputChange} />
-            <Button value="Create" onClick={this.buttonClick}>Create</Button>
+            <Input type="text" name="targetName" value={targetName} placeholder="Target Name" 
+                onChange={(e) => setTargetName(e.target.value)} />
+            <Input type="text" name="targetSlug" value={targetSlug} placeholder="Target Slug" 
+                onChange={(e) => setTargetSlug(e.target.value)} />
+            <Button value="Create" onClick={buttonClick}>Create</Button>
         </Card>
-        );
-    }
+    );
   }
   
   export default CreateTarget;

eacd92e74ee19b02c5b9d0e8ed58dec56d232e28
James Semple <jamessemple@SE-James-MBP.local>
2020-05-18 16:56:22 +0100

    Added targetRegion dropdown

--- src/CreateTarget.js
+++ src/CreateTarget.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Input from '@material-ui/core/Input';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import Card from '@material-ui/core/Card';
 import './CreateTarget.css';
 
 const CreateTarget = (props) => {
     const [targetName, setTargetName] = useState('');
     const [targetSlug, setTargetSlug] = useState('');
+    const [targetRegion, setTargetRegion] = useState('us-east-2');
 
     const click = props.cb;
 
@@ -25,6 +28,23 @@ const CreateTarget = (props) => {
                 onChange={(e) => setTargetName(e.target.value)} />
             <Input type="text" name="targetSlug" value={targetSlug} placeholder="Target Slug" 
                 onChange={(e) => setTargetSlug(e.target.value)} />
+            <Select name="targetRegion" id="targetRegion" value={targetRegion}
+                onChange={(e) => setTargetRegion(e.target.value)}>
+                <MenuItem value="us-east-1" selected>US East (N. Virginia)</MenuItem>
+                <MenuItem value="us-east-2">US East (Ohio)</MenuItem>
+                <MenuItem value="us-west-1">US West (N. California)</MenuItem>
+                <MenuItem value="us-west-2">US West (Oregon)</MenuItem>
+                <MenuItem value="ap-south-1">Asia Pacific (Mumbai)</MenuItem>
+                <MenuItem value="ap-northeast-2">Asia Pacific (Seoul)</MenuItem>
+                <MenuItem value="ap-southeast-1">Asia Pacific (Singapore)</MenuItem>
+                <MenuItem value="ap-southeast-2">Asia Pacific (Sydney)</MenuItem>
+                <MenuItem value="ap-northeast-1">Asia Pacific (Tokyo)</MenuItem>
+                <MenuItem value="ca-central-1">Canada (Central)</MenuItem>
+                <MenuItem value="eu-central-1">EU (Frankfurt)</MenuItem>
+                <MenuItem value="eu-west-1">EU (Ireland)</MenuItem>
+                <MenuItem value="eu-west-2">EU (London)</MenuItem>
+                <MenuItem value="eu-west-3">EU (Paris)</MenuItem>
+            </Select>
             <Button value="Create" onClick={buttonClick}>Create</Button>
         </Card>
     );
